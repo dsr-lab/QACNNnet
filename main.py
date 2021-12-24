2da5eb6b0001c26fe866ff7bea97bad4762432eb
Daniele Sirocchi <dsirocchi@gmail.com>
2021-12-24 16:55:57 +0100

    Highway network draft.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def main():
     print('main function')
-        
+
     a = np.asarray(
         [
             [
@@ -30,9 +30,9 @@ def main():
     BATCH_SIZE = 2
     N_WORDS = 2
     N_CHAR = 6
-    N_FILTER = 5
+    N_FILTER = 96  # blog=96
     EMB_SIZE = 20
-    CONV_SIZE = 3
+    CONV_SIZE = 3  # blog=5
     VOCAB_SIZE = 100
 
     sentence_input = np.random.randint(VOCAB_SIZE, size=(BATCH_SIZE, N_WORDS, N_CHAR))
@@ -56,7 +56,25 @@ def main():
     if (y.numpy() == y2.numpy()).all():
         print("equal!")
 
+    f = highway(y)
+
     print()
+
+
+def highway(x, activation=None, num_layers=2, dropout=0.0):
+
+    # size = kernel_number (last x dimension)
+    n_filters = x.shape[-1]  # output channels equal to input channels
+    kernel_size = 1
+
+    for i in range(num_layers):
+        # 96 in the paper (n_filters)
+        T = tf.keras.layers.Conv1D(n_filters, kernel_size, activation='sigmoid')(x)
+
+        H = tf.keras.layers.Conv1D(n_filters, kernel_size, activation=activation)(x)
+        H = tf.nn.dropout(H, dropout)
+        x = H * T + x * (1.0 - T)
+    return x
 
 
 if __name__ == '__main__':
