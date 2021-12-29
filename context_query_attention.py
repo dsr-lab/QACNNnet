Masking correctly implemented; fixed and tested all blocks

context_query_attention.py
@@ -3,8 +3,6 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-#TODO: handle masks
-
 class ContextQueryAttentionLayer (layers.Layer):
 
     def __init__(self, d_model):
@@ -17,8 +15,8 @@ class ContextQueryAttentionLayer (layers.Layer):
 
     def build_similarity_matrix(self, context, query):
 
-        n = tf.shape(context)[1]
-        m = tf.shape(query)[1]
+        n = int(tf.shape(context)[1])
+        m = int(tf.shape(query)[1])
 
         conc_layer = layers.Concatenate(axis=1)
         reshape_layer = layers.Reshape((n,m,self.d_model))
@@ -40,13 +38,22 @@ class ContextQueryAttentionLayer (layers.Layer):
 
         return similarity_matrix
 
-    def build_softmaxed_matrices (self, context, query, similarity_matrix):
+    def build_softmaxed_matrices (self, similarity_matrix, c_mask, q_mask):
+
+        n = int(tf.shape(similarity_matrix)[1])
+        m = int(tf.shape(similarity_matrix)[2])
+
+        repeated_q_mask = layers.RepeatVector(n) (q_mask)
+
+        reshaped_c_mask = layers.Reshape((n,1)) (c_mask)
+        repeated_c_mask = layers.Concatenate(axis=-1) ([reshaped_c_mask for _ in range(m)])
+        repeated_c_mask = layers.Reshape((n,m)) (repeated_c_mask)
 
         softmax_context_layer = layers.Softmax(axis=2)
         softmax_query_layer = layers.Softmax(axis=1)
 
-        context_softmaxed_matrix = softmax_context_layer(similarity_matrix)
-        query_softmaxed_matrix = softmax_query_layer(similarity_matrix)
+        context_softmaxed_matrix = softmax_context_layer(similarity_matrix, mask=repeated_q_mask)
+        query_softmaxed_matrix = softmax_query_layer(similarity_matrix, mask=repeated_c_mask)
 
         return context_softmaxed_matrix, query_softmaxed_matrix
 
@@ -90,7 +97,7 @@ class ContextQueryAttentionLayer (layers.Layer):
 
         similarity_matrix = self.build_similarity_matrix(context, query)
 
-        context_softmaxed_matrix, query_softmaxed_matrix = self.build_softmaxed_matrices(context, query, similarity_matrix)
+        context_softmaxed_matrix, query_softmaxed_matrix = self.build_softmaxed_matrices(similarity_matrix, c_mask, q_mask)
 
         context_to_query, query_to_context = self.build_attention_matrices(context, query, context_softmaxed_matrix, query_softmaxed_matrix)
 
@@ -98,8 +105,11 @@ class ContextQueryAttentionLayer (layers.Layer):
 
         return output
 
+#Test
 test = ContextQueryAttentionLayer(20)
-a = tf.constant(2,shape=(10,5,20),dtype=tf.float32)
-b = tf.constant(3,shape=(10,7,20),dtype=tf.float32)
-build = test([a,b],[None,None])
+a = tf.constant(2,shape=(1,5,20),dtype=tf.float32)
+b = tf.constant(3,shape=(1,7,20),dtype=tf.float32)
+c_mask = tf.convert_to_tensor([[True,True,False,False,False]])
+q_mask = tf.convert_to_tensor([[True,True,True,True,False,False, False]])
+build = test([a,b],[c_mask,q_mask])
 print(build)

encoder.py
@@ -61,7 +61,7 @@ class Encoding_Layer (layers.Layer):
 
         self_attention_layer_params = {
         "num_heads": n_heads,
-        "k_dim": d_model,
+        "key_dim": d_model,
         "kernel_regularizer": self.l2_decay
         }
 
@@ -81,11 +81,26 @@ class Encoding_Layer (layers.Layer):
 
         self.feed_forward_layer = layers.Dense(** feed_forward_layer_params) #Is one layer enough?
 
+    def compute_attention_mask(self, mask):
+
+        n = int(tf.shape(mask)[1])
+
+        horizontal_mask = layers.RepeatVector(n) (mask)
+
+        reshaped_mask = layers.Reshape((n,1)) (mask)
+        vertical_mask = layers.Concatenate(axis=-1) ([reshaped_mask for _ in range(n)])
+        vertical_mask = layers.Reshape((n,n)) (vertical_mask)
+
+        attention_mask = horizontal_mask & vertical_mask
+
+        return attention_mask
+
     def apply_layer(self,
                     x: tf.Tensor,
                     layer_num: int,
                     layer: layers.Layer,
-                    training: bool) -> tf.Tensor:
+                    training: bool,
+                    attention_mask = None) -> tf.Tensor:
 
         '''
         Check whether a layer should be used or not (stochastic dropout), then:
@@ -102,6 +117,8 @@ class Encoding_Layer (layers.Layer):
             Layer to be applied (one between SeparableConv1D, MultiHeadAttention or Dense)
         training: bool
             True if the model is training, False otherwise
+        attention_mask: tf.Tensor
+            Boolean mask used for MultiHeadAttention
 
         Returns:
         --------
@@ -112,12 +129,15 @@ class Encoding_Layer (layers.Layer):
         keep = stochastic_dropout.keep_layer(self.n_layers, layer_num, self.survival_prob) if training else True
         if keep:
             norm_x = self.norm_layers[layer_num](x)
-            f_x = layer(x)
-            return f_x + x
+            f_x = layer(x) if type(layer)!=layers.MultiHeadAttention else layer(x, x, attention_mask=attention_mask)
+            if int(tf.shape(f_x)[-1])==int(tf.shape(x)[-1]):
+                return f_x + x
+            else:
+                return f_x
         else:
             return x
 
-    def call(self, x, training):
+    def call(self, x, training, mask=None):
 
         '''
         Override of keras.layers.Layer method: computes the output of an Encoding Layer:
@@ -129,6 +149,8 @@ class Encoding_Layer (layers.Layer):
         The input of each layer is the residual block using a normalization layer to the output of the previous layer.
         '''
 
+        attention_mask = self.compute_attention_mask(mask)
+
         current_layer_num = 0
 
         #1. Apply positional encoding if it is the first block
@@ -137,21 +159,21 @@ class Encoding_Layer (layers.Layer):
             x *= tf.math.sqrt(tf.cast(self.embedding_size, tf.float32)) #Necessary?
             x += self.pos_encoding[:, :seq_len, :]
 
-        #2. Convolutional block
+        #2. Convolution block
         for conv_layer in self.conv_layers:
-            self.apply_layer(x, current_layer_num, conv_layer, training)
-            current_norm_layer+=1
+            x = self.apply_layer(x, current_layer_num, conv_layer, training)
+            current_layer_num+=1
 
         #3. Self-attention block
-        self.apply_layer(x, current_layer_num, self.self_attention_layer, training)
-        current_norm_layer+=1
+        x = self.apply_layer(x, current_layer_num, self.self_attention_layer, training, attention_mask=attention_mask)
+        current_layer_num+=1
 
         #4. Feed-forward block
-        self.apply_layer(x, current_layer_num, self.feed_forward_layer, training)
+        x = self.apply_layer(x, current_layer_num, self.feed_forward_layer, training)
 
         return x
 
-class Encoder (layers.Layer):
+class EncoderLayer (layers.Layer):
     def __init__(self,
                 embedding_size: int,
                 d_model: int,
@@ -187,7 +209,7 @@ class Encoder (layers.Layer):
             Number of encoding layers to stack
         '''
 
-        super(Encoder, self).__init__()
+        super(EncoderLayer, self).__init__()
 
         self.encoding_blocks = [Encoding_Layer(embedding_size,
         d_model,
@@ -199,7 +221,7 @@ class Encoder (layers.Layer):
         l2_value,
         i) for i in range(n_blocks)]
 
-    def call (self, x, training):
+    def call (self, x, training, mask=None):
 
         '''
         Override of keras.layers.Layer method: computes the output of an Encoder Layer,
@@ -207,6 +229,23 @@ class Encoder (layers.Layer):
         '''
 
         for encoding_block in self.encoding_blocks:
-            x = encoding_block(x, training)
+            x = encoding_block(x, training=training, mask=mask)
 
         return x
+
+#Test
+embedding_size = 500
+d_model = 128
+kernel_size=7
+n_conv_layers=4
+n_heads=2
+maximum_position_encoding = 1000
+survival_prob = 1
+l2_value = 0.005
+n_blocks = 1
+
+test = EncoderLayer(embedding_size, d_model, kernel_size, n_conv_layers, n_heads, maximum_position_encoding, survival_prob, l2_value, n_blocks)
+a = tf.constant(2,shape=(1,5,500),dtype=tf.float32)
+_mask = tf.convert_to_tensor([[True,True,True,False,False]])
+build = test(a,training=False, mask=_mask)
+print(build)

model_output.py
@@ -3,8 +3,6 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-#TODO: handle masks
-
 class OutputLayer (layers.Layer):
 
     def __init__ (self):
@@ -18,28 +16,38 @@ class OutputLayer (layers.Layer):
 
         self.softmax_layer = layers.Softmax(axis=-1)
 
-    def compute_probabilities (self, input_1, input_2, start):
+    def compute_probabilities (self, input_1, input_2, start, mask):
+
+        n = int(tf.shape(input_1)[1])
 
         concat = self.concatenate_layer([input_1, input_2])
 
         weighted = self.w1(concat) if start else self.w2(concat)
 
-        softmaxed = self.softmax_layer(weighted)
+        reshaped = layers.Reshape((n,)) (weighted)
 
-        n = tf.shape(input_1)[1]
-        softmaxed = layers.Reshape((n)) (softmaxed)
+        softmaxed = self.softmax_layer(reshaped, mask=mask)
 
         return softmaxed
 
-    def call (self, inputs):
+    def call (self, inputs, mask=None):
 
-        assert len(input)==3
+        assert len(inputs)==3
 
         m0 = inputs[0]
         m1 = inputs[1]
         m2 = inputs[2]
 
-        start_probbabilities = self.compute_probabilities(m0, m1, True)
-        end_probabilities = self.compute_probabilities(m0, m2, False)
+        start_probbabilities = self.compute_probabilities(m0, m1, True, mask)
+        end_probabilities = self.compute_probabilities(m0, m2, False, mask)
 
         return start_probbabilities, end_probabilities
+
+#Test
+test = OutputLayer()
+a = tf.constant(2,shape=(1,5,128),dtype=tf.float32)
+b = tf.constant(5,shape=(1,5,128),dtype=tf.float32)
+c = tf.constant(7,shape=(1,5,128),dtype=tf.float32)
+_mask = tf.convert_to_tensor([[True,True,True,False,False]])
+build = test([a,b,c], mask=_mask)
+print(build)