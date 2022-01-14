Exponential weight average fix.

model/question_answer_model.py
@@ -41,10 +41,11 @@ class QACNNnet(tf.keras.Model):
         self.dropout_rate = dropout_rate
         self.ema = tf.train.ExponentialMovingAverage(decay=0.9999)
 
-        self.model_is_training = False
+        self.model_is_training = None
         self.unaveraged_weights = None
 
     def call(self, inputs, training=None):
+
         assert len(inputs) == 4
 
         words_context = inputs[0]
@@ -81,13 +82,14 @@ class QACNNnet(tf.keras.Model):
 
     def train_step(self, data):
 
-        if not self.model_is_training:
-            self.model_is_training = True
+        # Restore unaveraged weights
+        if self.model_is_training == False:
             if self.unaveraged_weights is not None:
                 for idx, var in enumerate(self.trainable_variables):
-                    var.assign(self.unaveraged_weights[idx])
-                self.unaveraged_weights = None  
-        
+                    var.assign(tf.identity(self.unaveraged_weights[idx]))
+                self.unaveraged_weights = None
+        self.model_is_training = True
+
         # Unpack the data. Its structure depends on your model and
         # on what you pass to `fit()`.
         x, y = data
@@ -124,11 +126,19 @@ class QACNNnet(tf.keras.Model):
 
     def test_step(self, data):
 
-        if self.model_is_training:
-            self.unaveraged_weights = self.trainable_variables
-            self.model_is_training = False
+        # Save unaveraged weights and set the averaged ones
+        if self.model_is_training == True:
+            self.unaveraged_weights = []
             for var in self.trainable_variables:
+                # Deep copy the original variable
+                self.unaveraged_weights.append(tf.identity(var))
+
+                # Average the current variable
                 var.assign(self.ema.average(var))
+
+            # self.unaveraged_weights = self.trainable_variables
+
+        self.model_is_training = False
             
         # Unpack the data
         x, y = data