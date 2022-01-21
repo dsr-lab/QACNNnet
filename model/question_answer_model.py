54afabe349a795feffa0754ebbcb8e7211e89c1c
Daniele Sirocchi <dsirocchi@gmail.com>
2022-01-21 10:51:25 +0100

    Removed EMA. Increased some model parameters.

--- model/question_answer_model.py
+++ model/question_answer_model.py
@@ -90,13 +90,13 @@ class QACNNnet(tf.keras.Model):
 
     def train_step(self, data):
 
-        # Restore unaveraged weights
-        if self.model_is_training == False:
-            if self.unaveraged_weights is not None:
-                for idx, var in enumerate(self.trainable_variables):
-                    var.assign(tf.Variable(self.unaveraged_weights[idx]))
-                self.unaveraged_weights = None
-        self.model_is_training = True
+        # # Restore unaveraged weights
+        # if self.model_is_training == False:
+        #     if self.unaveraged_weights is not None:
+        #         for idx, var in enumerate(self.trainable_variables):
+        #             var.assign(tf.Variable(self.unaveraged_weights[idx]))
+        #         self.unaveraged_weights = None
+        # self.model_is_training = True
 
         # Unpack the data. Its structure depends on your model and
         # on what you pass to `fit()`.
@@ -122,7 +122,7 @@ class QACNNnet(tf.keras.Model):
         self.optimizer.apply_gradients(zip(capped_grads, trainable_vars))
 
         # Apply EMA
-        self.ema.apply(self.trainable_variables)
+        # self.ema.apply(self.trainable_variables)
 
         # Update the metrics
         self.loss_tracker.update_state(loss)
@@ -142,18 +142,18 @@ class QACNNnet(tf.keras.Model):
     def test_step(self, data):
 
         # Save unaveraged weights and set the averaged ones
-        if self.model_is_training == True:
-            self.unaveraged_weights = []
-            for var in self.trainable_variables:
-                # Deep copy the original variable
-                self.unaveraged_weights.append(tf.Variable(var))
-
-                # Average the current variable
-                var.assign(self.ema.average(var))
-
-            # self.unaveraged_weights = self.trainable_variables
-
-        self.model_is_training = False
+        # if self.model_is_training == True:
+        #     self.unaveraged_weights = []
+        #     for var in self.trainable_variables:
+        #         # Deep copy the original variable
+        #         self.unaveraged_weights.append(tf.Variable(var))
+        #
+        #         # Average the current variable
+        #         var.assign(self.ema.average(var))
+        #
+        #     # self.unaveraged_weights = self.trainable_variables
+        #
+        # self.model_is_training = False
 
         # Unpack the data
         x, y = data
